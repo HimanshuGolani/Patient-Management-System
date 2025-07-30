5c3078bd6683a93d19bd65ea29c206d549667a1f
HimanshuGolani <golanihimanshu@gmail.com>
2025-07-30 11:41:08 +0530

    Initial commit

---
+++ Pacient service/Pacient-service/src/main/java/com/pm/pacient_service/PacientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.pacient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacientServiceApplication.class, args);
+	}
+
+}
